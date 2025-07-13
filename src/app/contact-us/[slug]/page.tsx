e5b38bde1215838c9d9322a7951e6812ed0883e4
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-14 03:29:32 +0330

    remove unused routes

--- src/app/contact-us/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/products/[slug]/page.tsx
-import { useParams } from "@tanstack/react-router";
-
-export default function Slug() {
-  const { slug } = useParams({ strict: false }) as { slug: string };
-console.log(slug);
-  return <p>Slug: {slug}</p>;
-}
