59486e850cda3671e490921873b184a8923dde67
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-14 18:29:54 +0330

    use state for drop down in header

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -1,67 +1,77 @@
 import { Link, useNavigate } from "@tanstack/react-router";
-import { BiMoon } from "react-icons/bi";
+import { useState } from "react";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { IoSunnyOutline } from "react-icons/io5";
+import { IoMdMoon } from "react-icons/io";
 import { LuLogOut } from "react-icons/lu";
-import { MdOutlineMessage } from "react-icons/md";
+import { MdOutlineMessage, MdSunny } from "react-icons/md";
 import { PiUserCircleLight } from "react-icons/pi";
 import logo from "../../../assets/images/logo.png";
-import { toggleTheme } from "../../../utils/toggle-theme";
 import { useAuth } from "../../../context/auth-context";
+import { cn } from "../../../utils/cn";
+import { toggleTheme } from "../../../utils/toggle-theme";
 
 export default function Header() {
   const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
+
+  const [openDropdown, setOpenDropdown] = useState<null | "cart" | "account">(
+    null
+  );
+
   if (!isAuthenticated) return null;
+  const handleToggle = (dropdown: "cart" | "account") => {
+    setOpenDropdown((prev) => (prev === dropdown ? null : dropdown));
+  };
 
   return (
-    <header className="p-3">
-      <div className="rounded-lg p-2 gap-10 px-4 bg-primary-main dark:bg-slate-700 w-full backdrop-blur-md flex flex-wrap justify-between shadow-lg relative items-center">
+    <header className="p-3 absolute top-0 w-full z-50">
+      <div className="rounded-lg p-2 gap-10 px-4 bg-primary-400 dark:bg-slate-700 w-full backdrop-blur-md flex flex-wrap justify-between shadow-lg relative items-center">
         {/* Logo */}
         <Link to="/">
           <img
             src={logo}
             alt="logo"
-            className="brightness-0 invert-100 w-14 h-14"
+            className="brightness-0 invert-100 w-12 h-12"
           />
         </Link>
 
         {/* Right side */}
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-6 relative">
           {/* Theme Toggle */}
           <button
             onClick={toggleTheme}
-            className="dark:!text-primary-main text-white text-[28px]"
+            className="dark:!text-primary-main text-white pb-1.5 text-[28px]"
           >
-            <BiMoon className="hidden dark:block" />
-            <IoSunnyOutline className="dark:hidden" />
+            <IoMdMoon className="hidden dark:block" />
+            <MdSunny className="dark:hidden" />
           </button>
 
           {/* Cart Dropdown */}
-          <div
-            data-stats={4}
-            className="relative h-6 group focus-within:z-50 stats"
-          >
+          <div className="relative">
             <button
-              className="text-white dark:!text-primary-main text-2xl "
-              tabIndex={0}
+              onClick={() => handleToggle("cart")}
+              className="text-white dark:!text-primary-main text-2xl"
             >
               <FaShoppingCart />
             </button>
-            <button
-              className="absolute w-0 h-0 opacity-0 pointer-events-none"
-              aria-hidden
-            />
-            <div className="dropdown">
+            <div
+              className={cn(
+                "absolute left-0 top-8 w-52 bg-white dark:bg-slate-800 text-sm rounded-lg shadow-lg dark:shadow-2xl overflow-hidden transition-all duration-200 z-50",
+                openDropdown === "cart"
+                  ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
+                  : "opacity-0 scale-95 translate-y-2 pointer-events-none"
+              )}
+            >
               <div className="px-4 py-3 !text-primary-main text-lg font-semibold">
                 سبد خرید
               </div>
-              <div className="text-sm px-4 py-2 text-gray-500 dark:text-gray-300">
+              <div className="text-base px-4 py-2 text-gray-500 dark:text-gray-300">
                 هیچ محصولی اضافه نشده است.
               </div>
               <Link
                 to="/cart"
-                className="block px-4 py-2 text-center text-sm !text-primary-main hover:bg-gray-100 dark:hover:bg-slate-700"
+                onClick={() => setOpenDropdown(null)}
+                className="block px-4 py-2 text-center text-lg !text-primary-main hover:bg-gray-100 dark:hover:bg-slate-700"
               >
                 مشاهده سبد خرید
               </Link>
@@ -69,23 +79,24 @@ export default function Header() {
           </div>
 
           {/* Account Dropdown */}
-          <div
-            className="relative h-6 group focus-within:z-50 stats"
-            data-stats={10}
-          >
+          <div className="relative">
             <button
+              onClick={() => handleToggle("account")}
               className="text-white dark:!text-primary-main text-2xl"
-              tabIndex={0}
             >
               <FaUser />
             </button>
-            <button
-              className="absolute w-0 h-0 opacity-0 pointer-events-none"
-              aria-hidden
-            />
-            <div className="dropdown">
+            <div
+              className={cn(
+                "absolute left-0 top-8 w-56 bg-white dark:bg-slate-800 text-sm rounded-lg shadow-lg dark:shadow-2xl overflow-hidden transition-all duration-200 z-50",
+                openDropdown === "account"
+                  ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
+                  : "opacity-0 scale-95 translate-y-2 pointer-events-none"
+              )}
+            >
               <Link
                 to="/profile"
+                onClick={() => setOpenDropdown(null)}
                 className="flex items-center gap-2 px-4 py-2 !text-black dark:!text-white hover:bg-gray-100 dark:hover:bg-slate-600 text-xl"
               >
                 <PiUserCircleLight className="text-2xl" />
@@ -93,11 +104,12 @@ export default function Header() {
               </Link>
               <Link
                 to="/messages"
-                className="flex items-center gap-2 px-4 py-2 !text-black dark:!text-white hover:bg-gray-100 dark:hover:bg-slate-600 text-xl"
+                onClick={() => setOpenDropdown(null)}
+                className="flex items-center gap-2 px-4 py-2 !text-black dark:!text-white hover:bg-gray-100 dark:hover:bg-slate-600 text-xl relative"
               >
                 <MdOutlineMessage className="text-xl" />
                 <span>پیام‌ها</span>
-                <div className="left-4 absolute">
+                <div className="absolute top-1/2 left-6 translate-y-1/2">
                   <div className="stats" data-stats={10}></div>
                 </div>
               </Link>
@@ -105,6 +117,7 @@ export default function Header() {
                 onClick={() => {
                   logout();
                   navigate({ to: "/login" });
+                  setOpenDropdown(null);
                 }}
                 className="flex items-center gap-2 text-lg text-red-500 w-full text-right px-4 py-2 hover:bg-gray-100 dark:hover:bg-slate-600"
               >
