d61363e50ace3d04a98e0038f57ee64f9045dfa6
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-14 18:33:24 +0330

    update wrapping in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import Footer from "../components/ui/footer";
 import Header from "../components/ui/header";
 const Layout = () => {
   return (
-    <>
+    <div className="flex flex-col justify-between min-h-screen relative">
       <Header />
-      <main className="p-3">
+      <main className="p-3 mt-28">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
